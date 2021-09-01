93128caddd2cd731f0d3830b0f9b05da76112d80
Bethoven Acha <89837048+bethovenacha@users.noreply.github.com>
2021-09-01 20:44:31 +0800

    Add files via upload

---
+++ Policy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Creational.Singleton
+{
+    public class Policy
+    {
+        private static readonly Policy Instance = new Policy();
+        public static Policy instance{
+            get{
+                return Instance;
+            }
+        }
+        public Guid Id { get; set; }
+
+        public string Person { get; set; } = "John";
+        public string Name() => Person;
+    }
+}
